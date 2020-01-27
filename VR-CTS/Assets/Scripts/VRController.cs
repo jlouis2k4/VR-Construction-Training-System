95a5e240097ff004b83b1cd29aff305f5271d664
MCommins <michael77commins@gmail.com>
2020-01-27 15:15:09 -0800

    (Peter) Implemented fluid movement
    Was able to add fluid movement to the play allowing for player to walk left and right, as well as up and down smoothly.
    
    Co-Authored-By: XIZHI YANG <yangxizhi@users.noreply.github.com>

--- VR-CTS/Assets/Scripts/VRController.cs
+++ VR-CTS/Assets/Scripts/VRController.cs
@@ -7,7 +7,10 @@ public class VRController : MonoBehaviour
 {
     public float m_Sensitivity = 0.1f;
     public float m_MaxSpeed = 1.0f;
+    public float m_Gravity = 30.0f;
+    public float m_RotateIncrement = 90;
 
+    public SteamVR_Action_Boolean m_RotatePress = null;
     public SteamVR_Action_Boolean m_MovePress = null;
     public SteamVR_Action_Vector2 m_MoveValue = null;
 
@@ -35,56 +38,20 @@ public class VRController : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        HandleHead();
+    
         HandleHeight();
         CalculateMovement();
+        SnapRotateion();
 
     }
 
-    private void HandleHead()
-    {
-        //Store current
-        Vector3 oldPosition = m_CameraRig.position;
-        Quaternion oldRotation = m_CameraRig.rotation;
-        //Rotation
-        transform.eulerAngles = new Vector3(0.0f, m_Head.rotation.eulerAngles.y, 0.0f);
-        //Restore
-        m_CameraRig.position = oldPosition;
-        m_CameraRig.rotation = oldRotation;
-    }
-
-    private void CalculateMovement()
-    {
-        //Figure out movement orientation
-        Vector3 orientationEuler = new Vector3(0, transform.eulerAngles.y, 0);
-        Quaternion orientation = Quaternion.Euler(orientationEuler);
-        Vector3 movement = Vector3.zero;
-
-        //If not moving
-        if (m_MovePress.GetStateUp(SteamVR_Input_Sources.Any)) {
-            m_Speed = 0;
-        }
-
-        //If button pressed
-        if (m_MovePress.state) {
-            // Add, clamp
-            m_Speed += m_MoveValue.axis.y * m_Sensitivity;
-            m_Speed = Mathf.Clamp(m_Speed, -m_MaxSpeed, m_MaxSpeed);
-
-            // Orientation
-            movement += orientation * (m_Speed * Vector3.forward) * Time.deltaTime;
-        }
-
-        //Apply
-        m_CharacterController.Move(movement);
-    }
 
     private void HandleHeight()
     {
         //Get the head in local space
         float headHeight = Mathf.Clamp(m_Head.localPosition.y, 1, 2);
         m_CharacterController.height = headHeight;
-        
+
         //Cut in half
         Vector3 newCenter = Vector3.zero;
         newCenter.y = m_CharacterController.height / 2;
@@ -94,12 +61,59 @@ public class VRController : MonoBehaviour
         newCenter.x = m_Head.localPosition.x;
         newCenter.z = m_Head.localPosition.z;
 
-        // Rotate
-        newCenter = Quaternion.Euler(0, -transform.eulerAngles.y, 0) * newCenter;
 
         //Apply
         m_CharacterController.center = newCenter;
     }
+
+    private void CalculateMovement()
+    {
+        //Figure out movement orientation
+        
+        Quaternion orientation = CalculateOrientation();
+        Vector3 movement = Vector3.zero;
+
+        //If not moving
+        if (m_MoveValue.axis.magnitude == 0) {
+            m_Speed = 0;
+        }
+
+   
+        // Add, clamp
+        m_Speed += m_MoveValue.axis.magnitude * m_Sensitivity;
+        m_Speed = Mathf.Clamp(m_Speed, -m_MaxSpeed, m_MaxSpeed);
+
+        // Orientation and Gravity
+        movement += orientation * (m_Speed * Vector3.forward);
+        movement.y -= m_Gravity * Time.deltaTime;
+
+        //Apply
+        m_CharacterController.Move(movement * Time.deltaTime);
+    }
+
+
+    private Quaternion CalculateOrientation() {
+        float rotation = Mathf.Atan2(m_MoveValue.axis.x, m_MoveValue.axis.y);
+        rotation *= Mathf.Rad2Deg;
+
+        Vector3 orientationEuler = new Vector3(0, m_Head.eulerAngles.y + rotation, 0);
+        return Quaternion.Euler(orientationEuler);
+
+
+    }
+
+    private void SnapRotateion() {
+        float snapValue = 0.0f;
+
+        if (m_RotatePress.GetStateDown(SteamVR_Input_Sources.LeftHand))
+            snapValue = -Mathf.Abs(m_RotateIncrement);
+
+        if (m_RotatePress.GetStateDown(SteamVR_Input_Sources.RightHand))
+            snapValue = -Mathf.Abs(m_RotateIncrement);
+
+        transform.RotateAround(m_Head.position, Vector3.up, snapValue);
+    }
+
 
 
 }
