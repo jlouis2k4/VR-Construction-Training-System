f04807da41b851cdf2cd1f781caf7603796ed099
Michael Commins <michael77commins@gmail.com>
2020-01-31 11:24:17 -0800

    Added basic UI Scripts
    Implemented a pointer that extends from the right controller. The user can point at a UI element in a world-space canvas and select it.

---
+++ VR-CTS/Assets/Scripts/Pointer.cs
@@ -0,0 +1,49 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.EventSystems;
+
+public class Pointer : MonoBehaviour
+{
+	public float m_DefaultLength = 5.0f;
+	public GameObject m_Dot;
+	public VRInputModule m_InputModule;
+
+	private LineRenderer m_LineRenderer;
+
+	private void Awake()
+	{
+		m_LineRenderer = GetComponent<LineRenderer>();
+	}
+
+	private void Update()
+    {
+        UpdateLine();
+    }
+
+	private void UpdateLine() {
+		// Use default or distance
+		PointerEventData data = m_InputModule.GetData();
+		float targetLength = data.pointerCurrentRaycast.distance == 0 ? m_DefaultLength : data.pointerCurrentRaycast.distance;
+
+		// Raycast
+		RaycastHit hit = CreateRaycast(targetLength);
+
+		// Default
+		Vector3 endPosition = transform.position + (transform.forward * targetLength);
+
+		// Based on Hit
+		if (hit.collider != null) endPosition = hit.point;
+
+		// Set Linerenderer
+		m_LineRenderer.SetPosition(0, transform.position);
+		m_LineRenderer.SetPosition(1, endPosition);
+	}
+
+	private RaycastHit CreateRaycast(float length) {
+		RaycastHit hit;
+		Ray ray = new Ray(transform.position, transform.forward);
+		Physics.Raycast(ray, out hit, m_DefaultLength);
+		return hit;
+	}
+}
