da78bb3fabd74d78e6988dca68ee4289a241f48e
Michael Commins <michael77commins@gmail.com>
2020-02-01 12:59:53 -0800

    Added Player Icon UI
    Created a Player Icon Prefab containing a script to disable/enable sprites of safety gear on a silhouette.

---
+++ VR-CTS/Assets/Scripts/PlayerIcon.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class PlayerIcon : MonoBehaviour
+{
+    public Image m_HardHat;
+	public Image m_Goggles;
+	public Image m_Vest;
+
+    public void ToggleHardHat(bool isActive) {
+		m_HardHat.enabled = isActive;
+	}
+
+	public void ToggleGoggles(bool isActive) {
+		m_HardHat.enabled = isActive;
+	}
+
+	public void ToggleVest(bool isActive) {
+		m_HardHat.enabled = isActive;
+	}
+}
