f9a6b3e064f051441e235b1f91bb773ce652be0a
Steven Phillips <phillist@oregonstate.edu>
2020-03-02 12:24:22 -0800

    Player Hazard Interactions
    Player is able to interact with "low" tagged hazards (glasses,hat,vest) that displays to player they picked it up and destroys hazard. (only works for collision). Also, implemented Death for player. However, given that the "respawn" (teleporting) is being buggy for unknown reasons player will simply keep walking around from death position instead of respawing from specific point.

---
+++ VR-CTS/Assets/Hazard_Detection.cs
@@ -0,0 +1,38 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+public class Hazard_Detection : MonoBehaviour
+{
+
+    public Text myText;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+
+
+    void Update()
+    {
+
+    }
+
+    private void OnTriggerEnter(Collider other)
+    {
+        if(other.gameObject.tag == "low")
+        { 
+            print(other.gameObject.name);
+            print("\n)");
+            if(other.gameObject.name == "SafteyGlasses")
+            {
+                myText.text = other.gameObject.name.ToString() + " Picked Up!";
+                Destroy(other.gameObject);
+
+            }
+        }
+    }
+}
