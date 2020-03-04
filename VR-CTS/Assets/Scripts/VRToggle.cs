e426e11f69485260ee1f459c4a59836d053daee7
MCommins <michael77commins@gmail.com>
2020-03-03 16:49:01 -0800

    Added Toggling VR Between Scenes
    Added Toggle VR script that can be put in any scene do Enable/Disable OpenVR

---
+++ VR-CTS/Assets/Scripts/VRToggle.cs
@@ -0,0 +1,37 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.XR;
+
+public class VRToggle : MonoBehaviour
+{
+    public bool VREnabled;
+
+    public void Awake()
+    {
+        if (VREnabled)
+        {
+            EnableVR();
+        } else
+        {
+            DisableVR();
+        }
+    }
+
+    IEnumerator LoadDevice(string newDevice, bool enable)
+    {
+        XRSettings.LoadDeviceByName(newDevice);
+        yield return null;
+        XRSettings.enabled = enable;
+    }
+
+    void EnableVR()
+    {
+        StartCoroutine(LoadDevice("OpenVR", true));
+    }
+
+    void DisableVR()
+    {
+        StartCoroutine(LoadDevice("", false));
+    }
+}
