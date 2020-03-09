eef2cd4ef32f794930ccc34552757ccd4359d0a9
Steven Phillips <phillist@oregonstate.edu>
2020-03-08 19:24:08 -0700

    fixed bugs, implemented hazard interactions and pop-ups
    fixed some visual bugs with buildings, due to deletion of prefab. Create death canvas and hazard canavs when player collides with a tag "deathZone", or tag "low" for safety gear. panels and interactions need to be updated for VR. Also elevator hazard to block it has been implemented. all things mentioned before still need to be updated to hhazard compete script and score script.

--- VR-CTS/Assets/Scripts/Hazard_Detection.cs
+++ VR-CTS/Assets/Scripts/Hazard_Detection.cs
@@ -5,12 +5,19 @@ using UnityEngine.UI;
 public class Hazard_Detection : MonoBehaviour
 {
 
-    public Text myText;
+    public Text hazard_Panel;
+    public GameObject hazard_Menu_UI;
+    public GameObject death_Menu_UI;
+    private GameObject safety_item;
+    private bool pause_Game = false;
+
 
     // Start is called before the first frame update
     void Start()
     {
-        
+        death_Menu_UI.SetActive(false);
+        hazard_Menu_UI.SetActive(false);
+       
     }
 
     // Update is called once per frame
@@ -20,19 +27,83 @@ public class Hazard_Detection : MonoBehaviour
     {
 
     }
-
+    //checks to see if player collided with object of hazard, or 
+    //of death
+    /**    SCORE AND COMPLETENESS NEED TO BE IMPLEMENTED      **/
     private void OnTriggerEnter(Collider other)
     {
-        if(other.gameObject.tag == "low")
-        { 
-            print(other.gameObject.name);
-            print("\n)");
-            if(other.gameObject.name == "SafteyGlasses")
-            {
-                myText.text = other.gameObject.name.ToString() + " Picked Up!";
-                Destroy(other.gameObject);
-
-            }
+        //calls function death if player collides with death object
+        if (other.gameObject.tag == "deathZone")
+        {
+            game_Over();
         }
+
+        if (other.gameObject.tag == "low")
+        {
+            safety_Gear_Interact(other.gameObject.name, 0);   
+        }
+    }
+
+    private void safety_Gear_Interact(string other, float check_equip)
+    {
+        
+            print(other);
+            hazard_Panel.text = " Pick Up " + other.ToString() + "?!";
+            hazard_Menu_UI.SetActive(true);
+            Cursor.lockState = CursorLockMode.None;
+            Cursor.visible = true;
+            toggle_Time();
+      
+
+    }
+
+    public void equip()
+    {
+
+        toggle_Time();
+        hazard_Menu_UI.SetActive(false);
+        //destroy object and implement attaching to player;
+    }
+
+    public void dont_Equip()
+    {
+        toggle_Time();
+        hazard_Menu_UI.SetActive(false);
+        
+    }
+
+
+    public void game_Over()
+    {
+        //open death panel
+        death_Menu_UI.SetActive(true);
+        Cursor.lockState = CursorLockMode.None;
+        Cursor.visible = true;
+
+        //stop time....
+        toggle_Time();
+    }
+
+
+    private void toggle_Time()
+    {
+        pause_Game = !pause_Game;
+
+        if (pause_Game)
+        {
+            Time.timeScale = 0;
+        }
+        else
+        {
+            Time.timeScale = 1;
+        }
+    }
+
+    public void Retry()
+    {
+
+        toggle_Time();
+        death_Menu_UI.SetActive(false);
+
     }
 }
---
+++ VR-CTS/Assets/Scripts/elevator_Hazard.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class elevator_Hazard : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    /**    SCORE AND COMPLETENESS NEED TO BE IMPLEMENTED      **/
+
+    public void OnTriggerEnter(Collider other)
+    {
+        if(other.gameObject.tag == "barricade")
+        {
+            print(other.gameObject.name);
+            //call complete hazard function
+            //call score function
+        }
+    }
+}
